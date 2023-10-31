6bc825bb16f905cf8afb2502784f5eb3dfd3cd19
ambruthatn <149233279+ambruthatn@users.noreply.github.com>
2023-10-31 12:36:31 +0530

    Add files via upload

---
+++ ShowRoomStarter.java
@@ -0,0 +1,108 @@
+class ShowRoomStarter
+{
+	public static void main(String[] args)
+	{
+		
+	System.out.println("Starting of main ShowRoomStarter ");
+	
+	ShowRoom showRoom=new ShowRoom();
+	System.out.println("The name of the showroom is : :"+showRoom.name);
+	System.out.println("The no. of manager present is :"+showRoom.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom.noOfWorkers);
+	System.out.println("The type is :"+showRoom.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom.mostFamous);
+	System.out.println("The location is:"+showRoom.location);
+	
+	System.out.println("------------------------------------");
+	
+    ShowRoom showRoom1=new ShowRoom("Honda");
+	System.out.println("The name of the showroom is : :"+showRoom1.name);
+	System.out.println("The no. of manager present is :"+showRoom1.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom1.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom1.noOfWorkers);
+	System.out.println("The type is :"+showRoom1.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom1.mostFamous);
+	System.out.println("The location is:"+showRoom1.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom2=new ShowRoom(7);
+	System.out.println("The name of the showroom is : :"+showRoom2.name);
+	System.out.println("The no. of manager present is :"+showRoom2.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom2.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom2.noOfWorkers);
+	System.out.println("The type is :"+showRoom2.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom2.mostFamous);
+	System.out.println("The location is:"+showRoom2.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom3=new ShowRoom(1987.86);
+	System.out.println("The name of the showroom is : :"+showRoom3.name);
+	System.out.println("The no. of manager present is :"+showRoom3.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom3.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom3.noOfWorkers);
+	System.out.println("The type is :"+showRoom3.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom3.mostFamous);
+	System.out.println("The location is:"+showRoom3.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom4=new ShowRoom(4);
+	System.out.println("The name of the showroom is : :"+showRoom4.name);
+	System.out.println("The no. of manager present is :"+showRoom4.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom4.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom4.noOfWorkers);
+	System.out.println("The type is :"+showRoom4.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom4.mostFamous);
+	System.out.println("The location is:"+showRoom4.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom5=new ShowRoom("car show room",true);
+	System.out.println("The name of the showroom is : :"+showRoom5.name);
+	System.out.println("The no. of manager present is :"+showRoom5.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom5.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom5.noOfWorkers);
+	System.out.println("The type is :"+showRoom5.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom5.mostFamous);
+	System.out.println("The location is:"+showRoom5.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom6=new ShowRoom("Davangere","TVS");
+	System.out.println("The name of the showroom is : :"+showRoom6.name);
+	System.out.println("The no. of manager present is :"+showRoom6.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom6.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom6.noOfWorkers);
+	System.out.println("The type is :"+showRoom6.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom6.mostFamous);
+	System.out.println("The location is:"+showRoom6.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom7=new ShowRoom(4,9);
+	System.out.println("The name of the showroom is : :"+showRoom7.name);
+	System.out.println("The no. of manager present is :"+showRoom7.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom7.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom7.noOfWorkers);
+	System.out.println("The type is :"+showRoom7.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom7.mostFamous);
+	System.out.println("The location is:"+showRoom7.location);
+	
+	System.out.println("------------------------------------");
+	
+	ShowRoom showRoom8=new ShowRoom(true,6745.87);
+	System.out.println("The name of the showroom is : :"+showRoom8.name);
+	System.out.println("The no. of manager present is :"+showRoom8.noOfManagers);
+	System.out.println("The squareFeet In Meters is  :"+showRoom8.squareFeetInMeters);
+	System.out.println("The no. of workers is  :"+showRoom8.noOfWorkers);
+	System.out.println("The type is :"+showRoom8.typeOfShowroom);
+	System.out.println("The showroom is :"+showRoom8.mostFamous);
+	System.out.println("The location is:"+showRoom8.location);
+	
+	System.out.println("------------------------------------");
+	
+	}
+}
