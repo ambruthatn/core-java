6c124001f9d1b6e53ac5d5beb42c92ebc4505759
ambruthatn <ambruthatn.xworkz@gmail.com>
2023-11-06 11:24:08 +0530

    nov6

---
+++ Doctor.java
@@ -0,0 +1,8 @@
+class Doctor
+{
+	String name="Ambrutha";
+	int exp=1;
+	boolean fake=false;
+	int fees=400;
+	
+}
---
+++ Job.java
@@ -0,0 +1,6 @@
+class Job
+{
+	String role="Software Engineer";
+	int salary=26000;
+	String type="Full Time";	
+}
